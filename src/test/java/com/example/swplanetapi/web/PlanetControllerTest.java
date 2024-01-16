aa52f0a836cfe8d6b4c7f6a4118104ea3ce53608
Luan Valente <luanbvalente@hotmail.com>
2024-01-16 16:52:33 -0300

    Testes de Integração: Cenário de dados duplicados no Controller

--- src/test/java/com/example/swplanetapi/web/PlanetControllerTest.java
+++ src/test/java/com/example/swplanetapi/web/PlanetControllerTest.java
@@ -7,10 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static com.example.swplanetapi.common.PlanetConstans.PLANET;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -46,6 +48,14 @@ class PlanetControllerTest {
                 .andExpect(status().isUnprocessableEntity());
         mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(invalidPlanet)).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    public void createPlanet_WithExistingName_ReturnsConflict() throws Exception {
+        when(planetService.create(any())).thenThrow(DataIntegrityViolationException.class);
+
+        mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(PLANET)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict());
     }
 
 }
