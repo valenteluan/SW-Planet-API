1b39762c94ab59caad57bccf204eed482018afd8
Luan Valente <luanbvalente@hotmail.com>
2024-01-15 22:43:20 -0300

    Testes de Integração: Cenário de sucesso no Repository

---
+++ src/test/java/com/example/swplanetapi/domain/PlanetRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.example.swplanetapi.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static com.example.swplanetapi.common.PlanetConstans.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class PlanetRepositoryTest {
+    @Autowired
+    private PlanetRepository planetRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsPlanet() {
+        Planet planet = planetRepository.save(PLANET);
+
+        Planet sut = testEntityManager.find(Planet.class, planet.getId());
+
+        assertThat(sut).isNotNull();
+        assertThat(sut.getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+
+}
