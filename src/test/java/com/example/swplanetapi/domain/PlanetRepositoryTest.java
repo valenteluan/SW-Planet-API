2fa2b0efa64fa75e4a29a4acb32b4a84265e1c57
Luan Valente <luanbvalente@hotmail.com>
2024-01-16 21:37:51 -0300

    Exercício 1: testando a consulta de planeta por id

--- src/test/java/com/example/swplanetapi/domain/PlanetRepositoryTest.java
+++ src/test/java/com/example/swplanetapi/domain/PlanetRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.example.swplanetapi.domain;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.Optional;
 
 import static com.example.swplanetapi.common.PlanetConstans.PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,6 +19,11 @@ class PlanetRepositoryTest {
 
     @Autowired
     private TestEntityManager testEntityManager;
+
+    @AfterEach
+    public void afterEach() {
+        PLANET.setId(null);
+    }
 
     @Test
     public void createPlanet_WithValidData_ReturnsPlanet() {
@@ -45,6 +53,23 @@ class PlanetRepositoryTest {
         planet.setId(null);
 
         assertThatThrownBy(() -> planetRepository.save(planet)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void getPlanet_ByExistingId_ReturnsPlanet() {
+        Planet planet = testEntityManager.persistFlushFind(PLANET);
+
+        Optional<Planet> planetOpt = planetRepository.findById(planet.getId());
+
+        assertThat(planetOpt).isNotEmpty();
+        assertThat(planetOpt.get()).isEqualTo(planet);
+    }
+
+    @Test
+    public void getPlanet_ByUnexistingId_ReturnsEmpty() {
+        Optional<Planet> planetOpt = planetRepository.findById(1L);
+
+        assertThat(planetOpt).isEmpty();
     }
 
 }
--- src/test/java/com/example/swplanetapi/web/PlanetControllerTest.java
+++ src/test/java/com/example/swplanetapi/web/PlanetControllerTest.java
@@ -11,9 +11,12 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
 import static com.example.swplanetapi.common.PlanetConstans.PLANET;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -57,5 +60,19 @@ class PlanetControllerTest {
         mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(PLANET)).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isConflict());
     }
+
+    @Test
+    public void getPlanet_ByExistingId_ReturnsPlanet() throws Exception {
+        when(planetService.get(1L)).thenReturn(Optional.of(PLANET));
+
+        mockMvc.perform(get("/planets/1")).andExpect(status().isOk())
+                .andExpect(jsonPath("$").value(PLANET));
+    }
+
+    @Test
+    public void getPlanet_ByUnexistingId_ReturnsNotFound() throws Exception {
+        mockMvc.perform(get("/planets/1")).andExpect(status().isNotFound());
+    }
+
 
 }
