3667e5407addc3a347bc2f7c5574e5a9863afdc7
Luan Valente <luanbvalente@hotmail.com>
2024-01-17 21:27:51 -0300

    Exercício 2 - Testando a consulta de planeta por nome

--- src/test/java/com/example/swplanetapi/web/PlanetControllerTest.java
+++ src/test/java/com/example/swplanetapi/web/PlanetControllerTest.java
@@ -74,5 +74,20 @@ class PlanetControllerTest {
         mockMvc.perform(get("/planets/1")).andExpect(status().isNotFound());
     }
 
+    @Test
+    public void getPlanet_ByExistingName_ReturnsPlanet() throws Exception {
+        when(planetService.getByName(PLANET.getName())).thenReturn(Optional.of(PLANET));
+
+        mockMvc.perform(get("/planets/name/" + PLANET.getName()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value(PLANET));
+    }
+
+    @Test
+    public void getPlanet_ByUnexistingName_ReturnsNotFound() throws Exception {
+        mockMvc.perform(get("/planets/name/1"))
+                .andExpect(status().isNotFound());
+    }
+
 
 }
